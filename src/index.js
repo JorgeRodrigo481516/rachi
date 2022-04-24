86c98b903aaae962e56b45c0add3bca38d2c5553
Jorge Rodrigo <jorge.rodrigo.481516@gmail.com>
2022-04-24 12:55:53 -0300

    How it works has been completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './styles/sass/main.scss';
+
 import './assets/images/logo.svg';
+import './assets/images/person-plus.svg';
+import './assets/images/carbon-security.svg';
+import './assets/images/face.svg';
