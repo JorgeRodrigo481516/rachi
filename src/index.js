2367ae7a7efe006dc1a7b22a82f14979dcf698f5
Jorge Rodrigo <jorge.rodrigo.481516@gmail.com>
2022-04-25 14:21:01 -0300

    Add scroll behavior

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/sass/main.scss';
 
+import './assets/images/favicon.svg';
 import './assets/images/logo.svg';
 import './assets/images/person-plus.svg';
 import './assets/images/carbon-security.svg';
